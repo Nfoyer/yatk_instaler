375cc9294f30f91bce0fe13737e825ef87dc8b34
Nfoyer <103602535+Nfoyer@users.noreply.github.com>
2022-04-13 22:04:23 +0300

    Add files via upload

---
+++ yatk-master/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Пробник',
+    version='s',
+    packages=['code', 'code.sentiment', 'code.yatk-master', 'code.yatk-master.ml', 'code.yatk-master.thirdparty',
+              'code.Red-Pie-master', 'code.Red-Pie-master.red', 'code.Red-Pie-master.red.utils'],
+    url='s',
+    license='s',
+    author='dolot',
+    author_email='s',
+    description='s'
+)
